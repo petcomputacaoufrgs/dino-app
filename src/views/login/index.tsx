8748a776bf23e630434d9a70c79f27f23c0d24e4
Joao Pedro SIlveira <jpedross1999@gmail.com>
2020-03-04 11:12:46 -0300

    Feat(Login no frontend): Estrutura de login apenas para o frontend

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -1,15 +1,27 @@
 import React from 'react'
-import Button from "../../components/button"
+import GoogleLogin from 'react-google-login'
+import Button from '../../components/button'
+import GoogleSecret from '../../config/client_secret.json'
 import './styles.css'
 
 const Login = () => {
-    const login = () => {
-        console.log('logado');
+    const responseGoogle = (response: any) => {
+        console.log(response.tokenId);
+        alert(response.tokenId)
     }
 
     return (
         <div className='login'>
-            <Button onClick={login}>Entrar com o Google</Button>
+             <GoogleLogin
+                clientId={GoogleSecret.web.client_id}
+                buttonText="Entrar com o Google"
+                onSuccess={responseGoogle}
+                onFailure={responseGoogle}
+                cookiePolicy={'single_host_origin'}
+                render={renderProps => (
+                    <Button onClick={renderProps.onClick}>Entrar com o Google</Button>
+                  )}
+            />
         </div>
     )
 }

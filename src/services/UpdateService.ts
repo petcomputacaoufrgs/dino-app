a9c0a34e7e270d07213b0d8a7f44b15d6581417b
João <jpedross1999@gmail.com>
2020-04-15 02:32:40 -0300

    Fix(console.log): Remoção de log

--- src/services/UpdateService.ts
+++ src/services/UpdateService.ts
@@ -2,10 +2,8 @@ import GlossaryService from './GlossaryService'
 import AuthService from './GoogleAuthService'
 
 class UpdateService {
-
     checkUpdates = async () => {
         if(AuthService.isAuthenticated()){
-            console.log('updating...')
             GlossaryService.checkUpdate()
         }
     }

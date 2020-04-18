ece4f57dba76e13b9e215f9598da62b6c9597486
João <jpedross1999@gmail.com>
2020-04-18 14:50:43 -0300

    Feat(Loader): Modificação no loader para PACMAN
    Diminuir e simplificar código

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -1,16 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import LoaderSVG from '../../images/loader.svg'
+import PacmanLoader from "react-spinners/PacmanLoader"
 import LoaderProps from './props'
 import './styles.css'
-
-/* Classe principal básica do componente */
-const MainClass = 'loader'
-
-/* Classe principal para Loader inativo */
-const ClosedClass = 'loader__closed'
-
-/* Classe principal para Loader entrando em estado inativo */
-const EndingClass = 'loader__ending'
 
 /**
  * @description Loader de tela cheia
@@ -19,45 +10,24 @@ const Loader = (props: LoaderProps) => {
     /* Configura o estado de loading */
     const [loading, setLoading] = useState(props.loading)
 
-    /* Configura o estado de closed para finalizar a exibição do Loader */
-    const [closed, setClosed] = useState(!props.loading)
-
-    /* Atualiza o estado de loading pela mudança de propriedade*/
+    /* Atualiza o estado de loading pela mudança de propriedade */
     useEffect(() => {
-        if (loading) { 
-            if (!props.loading) { // Caso tenha mudado o estado de loading para false
-                setLoading(props.loading)
+        setLoading(props.loading)
+    }, [props.loading])
 
-                setClosed(true)
-            }
-        } else if (props.loading) { // Caso tenha mudado do estado de loading para true
-            setLoading(props.loading)
+    const getClass = (): string => {
+        if (loading) {
+            return 'loader'
         }
-    }, [loading, props.loading])
-
-    /**
-     * @description Retorna a classe principal baseado no estado atual do componente
-     */
-    const getMainClass = () => {
-        const className = MainClass
-
-        if (!loading) {
-            if (closed) {
-                return className.concat(' ').concat(ClosedClass)
-            } else {
-                return className.concat(' ').concat(EndingClass)
-            }
-        }
-
-        return className
+        return ''
     }
 
     return (
-        <div className={getMainClass()}>
-            <img 
-                className='loader__image' 
-                src={LoaderSVG} 
-                alt={props.alt ? props.alt : 'Imagem de círculo girando indicando carregamento.'}
+        <div className={getClass()}>
+            <PacmanLoader
+                size={40}
+                color={"#B32E55"}
+                loading={loading}
             />
         </div>
     )

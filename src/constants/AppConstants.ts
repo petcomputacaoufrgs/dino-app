a1932d02b0126ca9c2781ee41f58bcf1763d863d
Mayra <mayra.cademartori@gmail.com>
2020-05-18 19:19:28 -0300

    Client.json: Configurações magníficas; alterações incríveis

--- src/constants/AppConstants.ts
+++ src/constants/AppConstants.ts
@@ -2,10 +2,10 @@
  * @description Define nomes e valores padrões do app
  */
 class AppConstants {
-  /**
-   * @description Nome do aplicativo
-   */
-  APP_NAME: string = 'DinoApp'
+    /**
+     * @description Nome do aplicativo
+     */
+    APP_NAME: string = 'DinoApp'
 }
 
 export default new AppConstants()

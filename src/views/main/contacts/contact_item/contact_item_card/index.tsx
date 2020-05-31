122b2e3ed36aa38875f9ff4db440d1426d67dfc1
Mayra <mayra.cademartori@gmail.com>
2020-05-31 02:47:45 -0300

    Feat(Contact List Style Changes)

--- src/views/main/contacts/contact_item/contact_item_card/index.tsx
+++ src/views/main/contacts/contact_item/contact_item_card/index.tsx
@@ -4,14 +4,15 @@ import { Card, CardHeader, CardContent } from '@material-ui/core'
 import { MoreVert } from '@material-ui/icons'
 import useStyles from '../../styles'
 import ContactModel from '../../../../../services/contact/api_model/ContactModel'
+import { List, ListItem, ListItemText, ListItemIcon, Divider } from '@material-ui/core'
+import { Phone as PhoneIcon } from '@material-ui/icons/';
+
 
 
 const ContactItemCard = forwardRef(
   (props: { item: ContactModel }, ref): JSX.Element => {
 
     const classes = useStyles(props)
-
-    const phone = props.item.phone
 
     return (
       <Card className={classes.card} ref={ref}>
@@ -27,16 +28,32 @@ const ContactItemCard = forwardRef(
             </IconButton>
           }
           title={props.item.name}
-          subheader={`
-            ${phone.countryCode ? '+' + phone.countryCode + ' ' : ''}
-            ${phone.areaCode ? '(' + phone.areaCode + ') ' : ''}
-            ${phone.number}
-          `}
+          subheader={props.item.type}
         />
         <CardContent>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {props.item.description}
-          </Typography>
+          {props.item.description ?
+            <Typography variant="body2" color="textSecondary" component="p" style={{ paddingBottom: 10, paddingLeft: 5, paddingRight: 5 }}>
+              {props.item.description}
+            </Typography>
+            : <></>
+          }
+          <List component="nav" aria-label="main mailbox folders">
+            <Divider />
+
+            <ListItem button style={{ paddingTop: 10, paddingBottom: 0, paddingLeft: 5, paddingRight: 5 }}>
+              <ListItemIcon>
+                <PhoneIcon />
+              </ListItemIcon>
+              <ListItemText primary={
+                <Typography variant="subtitle1" color="textSecondary" component="p">
+                  {props.item.phone}
+                </Typography>}
+              />
+            </ListItem>
+          </List>
+
+
+
         </CardContent>
       </Card>
     )
--- src/views/main/contacts/contact_item/contact_item_list/index.tsx
+++ src/views/main/contacts/contact_item/contact_item_list/index.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import useStyles from '../../styles'
 import ContactItemListProps from './props'
-import { Avatar, ListItem, ListItemText, ListItemAvatar } from '@material-ui/core'
+import { Avatar, List, ListItem, ListItemText, ListItemAvatar } from '@material-ui/core'
 
 
 const ContactItemList = (props: ContactItemListProps): JSX.Element => {
 
-    const phone = props.item.phone
     const classes = useStyles(props)
 
     return (
@@ -16,11 +15,7 @@ const ContactItemList = (props: ContactItemListProps): JSX.Element => {
                     {props.item.name[0]}
                 </Avatar>
             </ListItemAvatar>
-            <ListItemText primary={props.item.name} secondary={`
-            ${phone.countryCode ? '+' + phone.countryCode + ' ' : ''}
-            ${phone.areaCode ? '(' + phone.areaCode + ') ' : ''}
-            ${phone.number}
-            `}
+            <ListItemText primary={props.item.name} secondary={props.item.type}
             />
         </ListItem>
     )

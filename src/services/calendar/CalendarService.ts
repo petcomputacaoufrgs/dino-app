f0abbfdeb96bb93cb84a943ef01261091f041d3b
Joao Pedro Silveira <jpedross1999@gmail.com>
2020-08-27 23:18:42 -0300

    Fix(Mock): Melhoria do mock de calendário

--- src/services/calendar/CalendarService.ts
+++ src/services/calendar/CalendarService.ts
@@ -41,7 +41,7 @@ class CalendarService {
     )
   }
 
-  addMocks = () => {
+  addMocks = async () => {
     const docs: EventDoc[] = [
       {
         color: '#4785E6',
@@ -53,87 +53,14 @@ class CalendarService {
         reminder_alarm_ms: 9200000,
         type: 0,
         _rev: '',
-      },
-      {
-        color: '#4785E6',
-        name: 'Consulta Oftalmo',
-        description:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.",
-        init_date: new Date(new Date().getTime() - 7200000),
-        end_date: new Date(new Date().getTime()),
-        reminder_alarm_ms: 9200000,
-        type: 0,
-        _rev: '',
-      },
-      {
-        color: '#4785E6',
-        name: 'Consulta Oftalmo',
-        description:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.",
-        init_date: new Date(new Date().getTime() - 7200000),
-        end_date: new Date(new Date().getTime()),
-        reminder_alarm_ms: 9200000,
-        type: 0,
-        _rev: '',
-      },
-      {
-        color: '#4285E6',
-        name: 'Consulta Oftalmo',
-        description:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.",
-        init_date: new Date(new Date().getTime() - 3600000),
-        end_date: new Date(new Date().getTime()),
-        reminder_alarm_ms: 9200000,
-        type: 0,
-        _rev: '',
-      },
-      {
-        color: '#4285E6',
-        name: 'Consulta Oftalmo',
-        description:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.",
-        init_date: new Date(new Date().getTime() - 4800000),
-        end_date: new Date(new Date().getTime()),
-        reminder_alarm_ms: 9200000,
-        type: 0,
-        _rev: '',
-      },
-      {
-        color: '#4285E6',
-        name: 'Consulta Oftalmo',
-        description:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.",
-        init_date: new Date(new Date().getTime() - 1600000),
-        end_date: new Date(new Date().getTime() + 44800000),
-        reminder_alarm_ms: 9200000,
-        type: 0,
-        _rev: '',
-      },
-      {
-        color: '#4285E6',
-        name: 'Consulta Oftalmo',
-        description:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.",
-        init_date: new Date(new Date().getTime()),
-        end_date: new Date(new Date().getTime() + 4800000),
-        reminder_alarm_ms: 9200000,
-        type: 0,
-        _rev: '',
-      },
-      {
-        color: '#4285E6',
-        name: 'Antes',
-        description:
-          "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.",
-        init_date: new Date(new Date().getTime()),
-        end_date: new Date(new Date().getTime() + 4800000),
-        reminder_alarm_ms: 192000000,
-        type: 0,
-        _rev: '',
-      },
+      }
     ]
 
     CalendarDatabase.putAll(docs)
+  }
+
+  removeUserData = () => {
+    CalendarDatabase.removeAll()
   }
 }
 

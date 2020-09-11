d78987fc1d9f959b6202b3a209536a6daa3e881c
Joao Pedro Silveira <jpedross1999@gmail.com>
2020-09-11 02:10:58 -0300

    Fix(FaqItem): Fix merge

--- src/views/main/faq/faq_items/index.tsx
+++ src/views/main/faq/faq_items/index.tsx
@@ -1,54 +1,34 @@
 import './styles.css'
 import React from 'react'
 import FaqItemsProps from './props'
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
+import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
 //import 'bootstrap/dist/css/bootstrap.min.css'
 import Card from 'react-bootstrap/Card'
 import Accordion from 'react-bootstrap/Accordion'
 
-const FaqItems = ({items, title}: FaqItemsProps): JSX.Element => {
-
-  return(
-      <div className='faq-items'>
-        <div className='faq-items__title'>
-          <Typography>{title}</Typography>
-          <Divider/>
-        </div>
-
-        <Accordion className="faq-items__accordion">
-          {items.map(item => (
-            <Card className="card" key={item.id}>
-              <Accordion.Toggle as={Card.Header} eventKey={item.id.toString()}>
-                <Typography>
-                  {item.question}
-                </Typography>
-              </Accordion.Toggle>
-              <Accordion.Collapse eventKey={item.id.toString()}>
-                <Card.Body>
-                  <Card.Text>{item.answer}</Card.Text>
-                </Card.Body>
-              </Accordion.Collapse>
-            </Card>
-          ))}
-        </Accordion>
+const FaqItems = ({ items, title }: FaqItemsProps): JSX.Element => {
+  return (
+    <div className="faq-items">
+      <div className="faq-items__title">
+        <Typography>{title}</Typography>
+        <Divider />
       </div>
-      {items.map((item) => (
-        <Accordion key={item.id}>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <Typography className={classes.heading}>
-              {item.question.toUpperCase()}
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>{item.answer}</Typography>
-          </AccordionDetails>
-        </Accordion>
-      ))}
+
+      <Accordion className="faq-items__accordion">
+        {items.map((item) => (
+          <Card className="card" key={item.id}>
+            <Accordion.Toggle as={Card.Header} eventKey={item.id.toString()}>
+              <Typography>{item.question}</Typography>
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey={item.id.toString()}>
+              <Card.Body>
+                <Card.Text>{item.answer}</Card.Text>
+              </Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        ))}
+      </Accordion>
     </div>
   )
 }

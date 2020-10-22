07c1a606e712332f4e99f94ea625254b7380c2ca
Mayra <mayra.cademartori@gmail.com>
2020-10-22 05:04:33 -0300

    WIP(Dynamic Background)

--- src/views/main/home/index.tsx
+++ src/views/main/home/index.tsx
@@ -11,9 +11,18 @@ const Home = () => {
         <div className='home__grid'>
               {[language.MENU_NOTES, language.MENU_GLOSSARY, language.MENU_FAQ, language.MENU_SETTINGS].map((item) => (
                     <ListItem button className='home__grid__button'>
-                        <Paper elevation={2} className='home__grid__paper'>
-                            <p className='home__paper__string'>{item}</p>
-                            <img src="https://s2.glbimg.com/z_gIOSUdsxyNGClgVLYVBHBziyw=/0x0:400x400/400x400/s.glbimg.com/po/tt2/f/original/2016/05/20/new-google-favicon-logo.png"></img>
+                        <Paper elevation={2} style={{
+                            width: '100%',
+                            height: 'calc(50vw - 18px)',
+                            backgroundImage: 'url(../../../assets/icons/menu_icons/glossary.svg)',
+                            backgroundSize: 'cover',
+                            backgroundColor: '#ADC2B9',
+                            display: 'grid',
+                            placeItems: 'center',
+                            fontSize: '5.5vmin',
+                            fontWeight: 'bold',
+                        }}>
+                              <p className='home__paper__string'>{item}</p>
                         </Paper>
                     </ListItem>
               ))}

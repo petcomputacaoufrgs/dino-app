0d616fb6b85638f6cbd2f79884af5be9a80da0cd
Mayra <mayra.cademartori@gmail.com>
2020-10-22 01:32:06 -0300

    Feat(Item Name)

--- src/views/main/home/index.tsx
+++ src/views/main/home/index.tsx
@@ -1,45 +1,22 @@
 import React from 'react'
 import { useCurrentLanguage } from '../../../context_provider/app_settings'
-import { Avatar, Typography } from '@material-ui/core'
-import { useUser } from '../../../context_provider/user'
 import './styles.css'
-import { Divider, Card, CardContent, CardHeader, IconButton, ListItem, List, Paper } from '@material-ui/core'
-import {ArrowRight} from '@material-ui/icons'
-import Grid, { GridSpacing } from '@material-ui/core/Grid';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
-
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      height: 140,
-      width: 100,
-    },
-    control: {
-      padding: theme.spacing(2),
-    },
-  }),
-);
+import { ListItem, Paper } from '@material-ui/core'
 
 const Home = () => {
   const language = useCurrentLanguage()
 
   return (
     <div className="home">
-      <div style={{padding:'15px', width:'100%'}}>
         <div className='home__grid'>
               {[language.MENU_NOTES, language.MENU_GLOSSARY, language.MENU_FAQ, language.MENU_SETTINGS].map((item) => (
                     <ListItem button style={{padding:'0px', height: '100%'}}>
                         <Paper className='home__paper'>
-                          <h5>{item}</h5>
+                            <p className='home__paper__string'>{item}</p>
                         </Paper>
                     </ListItem>
               ))}
         </div>
-      </div>
     </div>
   )
 }

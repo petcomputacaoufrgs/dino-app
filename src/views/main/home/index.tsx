a2c792d625e9f9932ec192a1d81835b09ba5e7d9
Mayra <mayra.cademartori@gmail.com>
2020-10-22 03:44:47 -0300

    Feat(<Img> opacity)

--- src/views/main/home/index.tsx
+++ src/views/main/home/index.tsx
@@ -10,9 +10,10 @@ const Home = () => {
     <div className="home">
         <div className='home__grid'>
               {[language.MENU_NOTES, language.MENU_GLOSSARY, language.MENU_FAQ, language.MENU_SETTINGS].map((item) => (
-                    <ListItem button style={{padding:'0px', height: '100%'}}>
-                        <Paper className='home__paper'>
+                    <ListItem button className='home__grid__button'>
+                        <Paper elevation={2} className='home__grid__paper'>
                             <p className='home__paper__string'>{item}</p>
+                            <img src="https://s2.glbimg.com/z_gIOSUdsxyNGClgVLYVBHBziyw=/0x0:400x400/400x400/s.glbimg.com/po/tt2/f/original/2016/05/20/new-google-favicon-logo.png"></img>
                         </Paper>
                     </ListItem>
               ))}

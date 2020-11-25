2c3d286f2d62d547906daa24557111afd5009302
Joao Pedro <jpedross1999@gmail.com>
2020-11-25 16:19:31 -0300

    Refactor(Pacotes): Limpeza e organização de pacotes

--- scripts/server.js
+++ scripts/server.js
@@ -2,11 +2,11 @@ const express = require('express')
 const compression = require('compression')
 const favicon = require('express-favicon')
 const path = require('path')
-const port = process.env.PORT || 3001
+const port = 3000
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'build')))
-app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
+app.use(express.static(path.join(__dirname, '../build')))
+app.use(favicon(path.join(__dirname, '../build', 'favicon.ico')))
 app.use(compression())
 
 app.use((req, res) => {
@@ -15,8 +15,11 @@ app.use((req, res) => {
   }
 })
 
+
 app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  res.sendFile(path.join(__dirname, '../build', 'index.html'))
 })
+
+console.log("SERVER PORT: " + port)
 
 app.listen(port)

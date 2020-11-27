d9c22abca3b9da9c7938b6c92529e0dbb79cd1bc
Victoria <victoria.ad40@gmail.com>
2020-11-27 11:07:03 -0300

    Feat(Terms of use view without css)

---
+++ src/views/privacy_policy/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './styles.css'
+
+const PrivacyPolicy: React.FC = () => {
+    return(
+        <div>Sou a política de serviços</div>
+    )
+}
+
+export default PrivacyPolicy

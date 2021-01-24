93421b7862bf1af7b5535910170315ae01979c8b
Mayra <mayra.cademartori@gmail.com>
2021-01-24 00:28:37 -0300

    Feat(Staff Moderation Panels)

--- src/views/staff_main/staff_moderation/index.tsx
+++ src/views/staff_main/staff_moderation/index.tsx
@@ -1,36 +1,41 @@
-import { TextField } from '@material-ui/core'
+import { Avatar, List, ListItem, ListItemAvatar, ListItemText, TextField } from '@material-ui/core'
 import React, { useState } from 'react'
 import Button from '../../../components/button'
 import { useLanguage } from '../../../context/language'
 import './styles.css'
 import { ReactComponent as SaveSVG } from '../../../assets/icons/save.svg'
-
+import DinoTabPanel from '../../../components/tab_panel'
+import AvatarIcon from '@material-ui/icons/Person';
 
 const StaffModeration: React.FC = () => {
 
   const language = useLanguage()
-
   const [email, setEmail] = useState('')
+  const [valueTab, setValueTab] = React.useState(0)
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChangeEmail = (event: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(event.target.value as string)
   }
 
   const handleAddEmail = () => {
-
+    setEmail('')
   }
 
-  return (
+
+  const renderFormContent = () => {
+    return (
     <div className='dialog_form__content'>
       <TextField
         required
         fullWidth
         value={email}
-        onChange={handleChange}
+        onChange={handleChangeEmail}
         margin='dense'
-        id='name'
+        id='email'
         label={language.data.FORM_EMAIL}
-        type='name'
+        type='email'
+        multiline
+        placeholder='Separe múltiplos e-mails por vírgula'
         inputProps={{ maxLength: 50 }}
         //error={isNameInvalid(props.name)}
       />
@@ -42,7 +47,43 @@ const StaffModeration: React.FC = () => {
         {language.data.FORM_ADD_STAFF}
       </Button>
     </div>
-  )
+    )
+  }
+
+  const renderListContent = () => {
+
+    const staff = [
+      { primary: 'mayra.cademartori@gmail.com', secondary: "Jan 9, 2014"},
+      { primary: 'aaaaaa.cademartori@gmail.com', secondary: "Jan 23, 2014"} 
+    ]
+
+    return (
+      <List>
+        {staff.map((e, index) => 
+          <ListItem key={index}>
+            <ListItemAvatar>
+              <Avatar><AvatarIcon /></Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={e.primary} secondary={e.secondary} />
+          </ListItem>
+        )}
+      </List>
+    )
+  }
+  
+
+  return (
+    <div className='staff_moderation'>
+      <DinoTabPanel 
+        valueTab={valueTab} 
+        setValueTab={setValueTab}
+        panels={[
+          { name: "Adicionar", Component: renderFormContent()}, 
+          { name: "Funcionários", Component: renderListContent()}
+        ]} 
+      />
+    </div>
+    )
 }
 
 export default StaffModeration

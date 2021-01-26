51fcd0841c7f16cefe824b2cac2bc99395543c35
Victoria <victoria.ad40@gmail.com>
2021-01-26 17:28:15 -0300

    Feat ( Game menu )

--- src/views/kids_space/dinogotchi/index.tsx
+++ src/views/kids_space/dinogotchi/index.tsx
@@ -2,20 +2,28 @@ import React from "react"
 import PathConstants from "../../../constants/app/PathConstants"
 import HistoryService from "../../../services/history/HistoryService"
 import IconButton from '../../../components/button/icon_button'
-//import Menu from "./menu"
-import { ReactComponent as Dino } from '../../../assets/doctor.svg'
-import { ReactComponent as GoBackSVG } from '../../../assets/go-back-arrow.svg'
+import CircularButton from '../../../components/button/circular_button'
+import Menu from "./menu"
+import { ReactComponent as Dino } from '../../../assets/kids_space/dinogotchi/doctor.svg'
+import { ReactComponent as GoBackSVG } from '../../../assets/kids_space/dinogotchi/go-back-arrow.svg'
+import { ReactComponent as GoOutSVG } from '../../../assets/kids_space/dinogotchi/exit.svg'
+import { ReactComponent as GameSVG } from '../../../assets/kids_space/dinogotchi/gamepad.svg'
 import './styles.css'
 
 // Adicionar <Menu> e tirar o resto depois
 
 const Dinogotchi: React.FC = () => {
     return(
-        <div className='classe_qualquer'> 
-            <div className = 'dinogotchi_header'>
+        <div className='dinogotchi_screen'>
+            <Menu />
+            <div className = 'dinogotchi_screen__header'>
                 <IconButton icon = {GoBackSVG} onClick = {() => {HistoryService.push(PathConstants.HOME)}}/>
             </div>
-            <Dino className='dino_pet'/>
+            <div className='dinogotchi_screen__options'>
+                <CircularButton icon = {GameSVG} onClick = {() => console.log('oizi')} />
+                <CircularButton icon = {GoOutSVG} onClick = {() => console.log('oizi')} />
+            </div>
+            <Dino className='dinogotchi_screen__dino_pet'/>
         </div>
     )
 }

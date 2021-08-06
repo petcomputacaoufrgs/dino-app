02db5a610fdef83a4302bc50c67aef0e39695eee
Victoria <victoria.ad40@gmail.com>
2021-08-06 09:36:35 -0300

    Fix: status bar height

--- src/components/status_indicator/index.tsx
+++ src/components/status_indicator/index.tsx
@@ -15,16 +15,16 @@ const StatusIndicator: React.FC<StatusIndicatorProps> = ({ fillHealth, fillEnerg
     return (
         <div className='status_wrapper dino__flex_row'>
             <div className='status_item'>
-                <div className='status_bg status_health' style={{ clipPath: `circle(50% at 50% ${fillHealth - 50}%`, height: `${fillHealth}` }}></div>
+                <div className='status_bg status_health' style={{ clipPath: `circle(50% at 50% ${fillHealth - 50}%`, height: `${fillHealth}%`}}></div>
                 <HealthStatus />
 
             </div>
             <div className='status_item'>
-                <div className='status_bg status_energy' style={{ clipPath: `circle(50% at 50% ${fillEnergy - 50}%`, height: `${fillEnergy}` }}></div>
+                <div className='status_bg status_energy' style={{ clipPath: `circle(50% at 50% ${fillEnergy - 50}%`, height: `${fillEnergy}%`}}></div>
                 <EnergyStatus />
             </div>
             <div className='status_item'>
-                <div className='status_bg status_happiness' style={{ clipPath: `circle(50% at 50% ${fillHappiness - 50}%`, height: `${fillHappiness}` }}></div>
+                <div className='status_bg status_happiness' style={{ clipPath: `circle(50% at 50% ${fillHappiness - 50}%`, height: `${fillHappiness}%`}}></div>
                 <HappinessStatus />
             </div>
         </div>

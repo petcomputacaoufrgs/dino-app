9dc19ceb88a9d381591b367e3a44d61e993ea327
Mayra <mayra.cademartori@gmail.com>
2021-08-19 14:04:08 -0300

    Feat(new loadUserData for building day and event views)

--- src/views/main/calendar/index.tsx
+++ src/views/main/calendar/index.tsx
@@ -1,52 +1,115 @@
 import React, { useEffect, useState } from 'react'
 import AddButton from '../../../components/button/icon_button/add_button'
 import { useLanguage } from '../../../context/language'
-import CalendarEventEntity from '../../../types/calendar/database/CalendarEventEntity'
 import MonthNavBar from '../../../components/calendar/month_nav_bar'
 import './styles.css'
 import GoogleScopeService from '../../../services/auth/google/GoogleScopeService'
 import CalendarEventService from '../../../services/calendar/CalendarEventService'
 import UserSettingsService from '../../../services/user/UserSettingsService'
-import CalendarEntity from '../../../types/calendar/database/CalendarEntity'
 import UserSettingsEntity from '../../../types/user/database/UserSettingsEntity'
 import CRUDEnum from '../../../types/enum/CRUDEnum'
 import { EventDialogForm } from './event_dialog_form'
 import { GoogleCalendarGrantDialog } from '../../../components/dialogs/google_grant_dialog'
 import CalendarDay from '../../../components/calendar/calendar_day'
-import { EventView } from '../../../components/calendar/calendar_event'
 import CalendarSettings from '../../../components/calendar/calendar_settings'
 import DinoTabPanel from '../../../components/tab_panel'
-import { IndeterminateCheckBoxTwoTone } from '@material-ui/icons'
 import CalendarEventTypeEntity from '../../../types/calendar/database/CalendarEventTypeEntity'
+import {
+	CalendarDayView,
+	CalendarEventView,
+} from '../../../types/calendar/view/CalendarView'
+import CalendarEventTypeService from '../../../services/calendar/CalendarEventTypeService'
 
 const Calendar: React.FC = () => {
 	const language = useLanguage()
 	const [isLoading, setIsLoading] = useState(true)
-	//const [events, setEvents] = useState<CalendarEventEntity[]>([])
+	const [dayViews, setDayViews] = useState<CalendarDayView[]>()
+	const [eventTypes, setEventTypes] = useState<CalendarEventTypeEntity[]>()
 	const [settings, setSettings] = useState<UserSettingsEntity>()
 	const [openGrantDialog, setOpenGrantDialog] = useState(false)
 	const [toAction, setToAction] = useState(CRUDEnum.NOP)
-	const [selectedItem, setSelectedItem] = useState<EventView>()
+	const [selectedItem, setSelectedItem] = useState<CalendarDayView>()
 
-	const mockCalendarDays = [
-		{
-			dayOfMonth: '01',
-			events: [
-				{ name: 'Lembrete Top', time: '10:00 - 11:00', color: '#d783bb' },
-				{ name: 'Eventão', time: '9:00 - 22:00', color: '#d1810d' },
-			],
-		},
-		{
-			dayOfMonth: '02',
-			events: [],
-		},
-	]
+	useEffect(() => {
+		const loadUserData = async (): Promise<CalendarDayView[]> => {
+			const syncGoogleCalendar = await GoogleScopeService.hasCalendarGrant()
+			if (settings && !settings.declineGoogleCalendar) {
+				if (!syncGoogleCalendar) setOpenGrantDialog(true)
+			}
+			const events = await CalendarEventService.getAll()
+			const types = await CalendarEventTypeService.getAll()
+
+			setEventTypes(types)
+
+			const month = new Array<CalendarDayView>(31)
+			for (let index = 0; index < 31; index++) {
+				month[index] = { dayOfMonth: `${index + 1}`, events: [] }
+			}
+
+			events.forEach(e => {
+				const type = types.find(t => t.localId === e.typeLocalId)
+
+				const eventView: CalendarEventView = {
+					title: e.title,
+					description: e.description,
+					time: e.time,
+					color: type?.color,
+				}
+
+				const index = Number(e.date) - 1
+				month[index].events.push(eventView)
+			})
+
+			return month
+		}
+
+		const loadData = async () => {
+			const settings = await UserSettingsService.getFirst()
+			updateSettings(settings)
+
+			const dayViews = await loadUserData()
+
+			updateDayViews(dayViews)
+			finishLoading()
+		}
+
+		CalendarEventService.addUpdateEventListenner(loadData)
+		UserSettingsService.addUpdateEventListenner(loadData)
+		GoogleScopeService.addUpdateEventListenner(loadData)
+
+		let updateDayViews = (dayViews: CalendarDayView[]) => {
+			setDayViews(dayViews)
+		}
+
+		let updateSettings = (settings: UserSettingsEntity | undefined) => {
+			setSettings(settings)
+		}
+
+		let finishLoading = () => {
+			setIsLoading(false)
+		}
+
+		if (isLoading) {
+			loadData()
+		}
+
+		return () => {
+			updateDayViews = () => {}
+			updateSettings = () => {}
+			finishLoading = () => {}
+			CalendarEventService.removeUpdateEventListenner(loadData)
+			UserSettingsService.removeUpdateEventListenner(loadData)
+			GoogleScopeService.removeUpdateEventListenner(loadData)
+		}
+	}, [isLoading])
+
+	const handleClose = () => setToAction(CRUDEnum.NOP)
 
 	const renderCalendar = () => {
 		return (
 			<div className='calendar'>
 				<MonthNavBar />
-				{mockCalendarDays.map((e, index) => (
+				{dayViews?.map((e, index) => (
 					<CalendarDay
 						key={index}
 						dayOfMonth={e.dayOfMonth}
@@ -71,76 +134,13 @@ const Calendar: React.FC = () => {
 		)
 	}
 
-	useEffect(() => {
-		const loadUserData = async (): Promise<CalendarEventEntity[]> => {
-			const syncGoogleCalendar = await GoogleScopeService.hasCalendarGrant()
-			if (settings && !settings.declineGoogleCalendar) {
-				if (!syncGoogleCalendar) setOpenGrantDialog(true)
-			}
-			return CalendarEventService.getAll()
-		}
-
-		const loadData = async () => {
-			const settings = await UserSettingsService.getFirst()
-			updateSettings(settings)
-
-			const events = await loadUserData()
-			events.push({ title: 'mock title' })
-
-			updateEvents(events)
-			finishLoading()
-		}
-
-		CalendarEventService.addUpdateEventListenner(loadData)
-		UserSettingsService.addUpdateEventListenner(loadData)
-		GoogleScopeService.addUpdateEventListenner(loadData)
-
-		let updateEvents = (events: CalendarEventEntity[]) => {
-			//setEvents(events)
-		}
-
-		let updateSettings = (settings: UserSettingsEntity | undefined) => {
-			setSettings(settings)
-		}
-
-		let finishLoading = () => {
-			setIsLoading(false)
-		}
-
-		if (isLoading) {
-			loadData()
-		}
-
-		return () => {
-			updateEvents = () => {}
-			updateSettings = () => {}
-			finishLoading = () => {}
-			CalendarEventService.removeUpdateEventListenner(loadData)
-			UserSettingsService.removeUpdateEventListenner(loadData)
-			GoogleScopeService.removeUpdateEventListenner(loadData)
-		}
-	}, [isLoading])
-
-	const handleClose = () => setToAction(CRUDEnum.NOP)
-
-	const mockEventTypes = [
-		{ icon: 'calendar', title: 'Evento', color: '#afd153', isUniversal: 1 },
-		{ icon: 'faq', title: 'Lembrete', color: '#ab1e53', isUniversal: 1 },
-		{
-			icon: 'faq',
-			title: 'Personalizadooooo',
-			color: '#ab5e13',
-			isUniversal: 0,
-		},
-	] as CalendarEventTypeEntity[]
-
 	return (
 		<DinoTabPanel
 			panels={[
 				{ Label: language.data.MENU_CALENDAR, Component: renderCalendar() },
 				{
 					Label: language.data.MENU_SETTINGS,
-					Component: <CalendarSettings eventTypes={mockEventTypes} />,
+					Component: <CalendarSettings eventTypes={eventTypes} />,
 				},
 			]}
 		/>

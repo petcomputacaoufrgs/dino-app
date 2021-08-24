35881953d3685cc4a93baebef2f3f0a37974f254
Leo <leohernandesvasc@gmail.com>
2021-08-24 19:35:19 -0300

    fix(Reduced numbers size)

--- src/views/kids_space/game_menu/dino_slider/components/piece/index.tsx
+++ src/views/kids_space/game_menu/dino_slider/components/piece/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './styles.css'
 
 const SliderPiece: React.FC<{num: number, reduced?: boolean}> = ({num, reduced}) => 
-    <div className={`dino_slider__piece piece_${num}` + (reduced ? ' reduced' : '')}>
+    <div className={'dino_slider__piece' + (reduced ? ` reduced piece_${num}` : ` piece_${num}`)}>
         {num !== 0 
             ? reduced 
                 ? Math.log2(num) 

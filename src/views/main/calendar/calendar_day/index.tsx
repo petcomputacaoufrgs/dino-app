7d4dff666ee6ad0f485d79e9a779c08dadaf3c44
Mayra <mayra.cademartori@gmail.com>
2021-08-31 16:56:42 -0300

    Feat(on click empty space with no events, open form for new event)

--- src/views/main/calendar/calendar_day/index.tsx
+++ src/views/main/calendar/calendar_day/index.tsx
@@ -1,35 +1,37 @@
 import React from 'react'
 import { useLanguage } from '../../../../context/language'
-import { CalendarEventView } from '../../../../types/calendar/view/CalendarView'
+import {
+	CalendarDayView,
+	CalendarEventView,
+} from '../../../../types/calendar/view/CalendarView'
+import DateUtils from '../../../../utils/DateUtils'
 import CalendarEvent from '../calendar_event'
 import './styles.css'
 
 const CalendarDay: React.FC<{
-	dayOfWeek: string
-	dayOfMonth: string
-	events: CalendarEventView[]
-	onClick: (item: CalendarEventView) => void
+	item: CalendarDayView
+	onClickEvent: (item: CalendarEventView) => void
+	onClickEmpty: (dayOfMonth: string) => void
 }> = props => {
 	const language = useLanguage()
 
 	return (
 		<div className='day_wrapper dino__flex_row'>
 			<div className='day_number_wrapper'>
-				<p className='week_day'>{props.dayOfWeek}</p>
-				<p className='month_day'>
-					{props.dayOfMonth.length === 1
-						? '0' + props.dayOfMonth
-						: props.dayOfMonth}
-				</p>
+				<p className='week_day'>{props.item.dayOfWeek}</p>
+				<p className='month_day'>{props.item.dayOfMonth}</p>
 			</div>
 			<div className='day_event_list_wrapper'>
-				{props.events.length === 0 ? (
-					<div className='empty_event_list'>
+				{props.item.events.length === 0 ? (
+					<div
+						className='empty_event_list'
+						onClick={() => props.onClickEmpty(props.item.dayOfMonth)}
+					>
 						<p>{language.data.EMPTY_EVENT_LIST}</p>
 					</div>
 				) : (
-					props.events.map((e, index) => (
-						<CalendarEvent key={index} item={e} onClick={props.onClick} />
+					props.item.events.map((e, index) => (
+						<CalendarEvent key={index} item={e} onClick={props.onClickEvent} />
 					))
 				)}
 			</div>
